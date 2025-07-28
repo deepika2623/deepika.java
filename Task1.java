cf38ec7b9eddad1917174f7a27e0c1b0302cbf25
deepika2623 <reddideepika20@gmail.com>
2025-07-28 21:29:25 +0530

    operators

---
+++ Task1.java
@@ -0,0 +1,20 @@
+public class Task1 {
+    public static void main(String[] args) {
+        String AccountholderName = "Deepika";
+        int AccountBalance = 200000;
+        int numberOfTransactions = 4;
+        int depositAmount = 9000;
+        int withdrawalAmount = 5000;
+        boolean isAccountActive = true;
+
+        double currentBalance = AccountBalance + depositAmount - withdrawalAmount;
+        boolean depositAmountAndAccountActive = depositAmount > 0 && isAccountActive;
+        boolean withdrawalAmountAndAccountActive = withdrawalAmount > 0 && isAccountActive;
+        numberOfTransactions++;
+
+        //Displaying the account details
+        System.out.println("Account Holder Name: " + AccountholderName);
+        System.out.println("Final Balance: " + currentBalance);
+        System.out.println("Number of Transactions: " + numberOfTransactions);
+    }
+}
